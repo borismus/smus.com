4c5b73286ea33f54a70630b34c7d8773746b358b
Boris Smus <boris@smus.com>
2019-05-24 10:18:45 -0700

    fix deploys cript

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
 DEPLOY_PATH=$HOME/Projects/smus.com-deploy
+LIGHTNING=$PWD/../lightning/lightning
+LIGHTNING_CONFIG=$PWD/lightning.yaml
 
-# Do a deploy build to the smus.com gh-pages repo.
-../lightning/lightning --out=$DEPLOY_PATH
-
-# Commit the updated contents there, and push it upstream.
 pushd $DEPLOY_PATH
 
+# Reset the gh-pages branch to master.
 git reset --hard origin/gh-pages
 git pull origin gh-pages
+# Do a production build to the gh-pages repo.
+$LIGHTNING -b $LIGHTNING_CONFIG -o=$DEPLOY_PATH
+# Add all of the things, commit and push to the repo.
 git add -A
 git commit -m "Updating smus.com with new content."
 git push origin gh-pages
