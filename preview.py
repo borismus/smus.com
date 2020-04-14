2bdbb9bf12cba9bb36123d230c16d1c43f96d9c0
Boris Smus <boris@smus.com>
2020-04-13 21:20:53 -0700

    Now using ~ instead of absolute path.

--- preview.py
+++ preview.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from livereload import Server, shell
+import os
 
-ROOT = '/Users/smus/Blog/www'
-LIGHTNING = '/Users/smus/Projects/lightning/lightning'
+ROOT = os.path.expanduser('~/Blog/www')
+LIGHTNING = os.path.expanduser('~/Projects/lightning/lightning')
 server = Server()
 server.watch('content', shell('%s -o %s' % (LIGHTNING, ROOT)))
 server.serve(root=ROOT)
