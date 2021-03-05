42bf3dcf5845b693f3ed453866fd57246ab9b1f6
Boris Smus <boris@smus.com>
2021-03-04 16:55:10 -0800

    only reload on changes to .md

--- preview.py
+++ preview.py
@@ -5,5 +5,5 @@ import os
 ROOT = os.path.expanduser('~/Blog/www')
 LIGHTNING = os.path.expanduser('~/Projects/lightning/lightning')
 server = Server()
-server.watch('content', shell('%s -o %s' % (LIGHTNING, ROOT)))
+server.watch('content/**.md', shell('%s -o %s' % (LIGHTNING, ROOT)))
 server.serve(root=ROOT)
