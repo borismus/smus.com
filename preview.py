b3646db2acc74581fd0e738d8e68f285b4619d9f
Boris Smus <boris@smus.com>
2021-06-21 10:06:51 -0700

    added more granular livereload

--- preview.py
+++ preview.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 from livereload import Server, shell
 import os
+import formic
 
 ROOT = 'preview-www'
 LIGHTNING = os.path.expanduser('~/Projects/lightning/lightning')
+
+BUILD_COMMAND = shell('%s -o %s' % (LIGHTNING, ROOT))
+
 server = Server()
-server.watch('content', shell('%s -o %s' % (LIGHTNING, ROOT)))
+
+for filepath in formic.FileSet(include="**.md"):
+  server.watch(filepath, BUILD_COMMAND)
+
 server.serve(root=ROOT)
