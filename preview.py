ceec33d703d722b7cd9a6359b30e79604e368c15
Boris Smus <boris@smus.com>
2021-06-30 23:33:23 -0700

    attempt to make more granular watches

--- preview.py
+++ preview.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
 from livereload import Server, shell
 import os
-import formic
 
 ROOT = 'preview-www'
 LIGHTNING = os.path.expanduser('~/Projects/lightning/lightning')
-
-BUILD_COMMAND = shell('%s -o %s' % (LIGHTNING, ROOT))
-
 server = Server()
-
-for filepath in formic.FileSet(include="**.md"):
-  server.watch(filepath, BUILD_COMMAND)
-
+server.watch('content', shell('%s -o %s' % (LIGHTNING, ROOT)))
 server.serve(root=ROOT)
